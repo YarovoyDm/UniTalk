f903822bab1af3a3b1661a31eeee482ef38dc511
YarovoyDM <yarovoy.dmytro@gmail.com>
2024-08-22 14:06:29 +0300

    performance optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useAppDispatch, useAppSelector } from './store';
 import { getOperatorsStart } from './store/slices/operator';
 import { getOperatorsAddonStart } from './store/slices/operatorAddon';
 import { GridColDef } from '@mui/x-data-grid';
 import moment from 'moment'
-import CheckBoxCell from './ui/cells/Checkbox/Checkbox';
 import { ColumnsType } from './types/types';
 import Table from './components/Table/Table';
-import {
-  STATIC_FIELD_CREATED_AT,
-  STATIC_FIELD_ID,
-  STATIC_FIELD_IS_WORKING,
-  STATIC_FIELD_NAME,
-  STATIC_HEADER_NAME_CREATING_DATE,
-  STATIC_HEADER_NAME_ID,
-  STATIC_HEADER_NAME_USER,
-  STATIC_HEADER_NAME_WORKING
-} from './constants';
 import styled from 'styled-components';
+import { STATIC_COLUMNS } from './constants';
 
 type RowsType = {
   id: string;
   name: string;
   isWorking: boolean;
   createdAt: string;
-  [key: string]: any;
+  [key: string]: string | boolean;
 };
 
 const Wrapper = styled.div`
@@ -38,7 +28,7 @@ const App = () => {
     const dispatch = useAppDispatch();
     const operators = useAppSelector(state => state.operatorsReducer.operators);
     const operatorsAddon = useAppSelector(state => state.operatorsAddonReducer.operatorsAddon).filter((item, index, self) =>
-        index === self.findIndex((t) => t.fieldName === item.fieldName)
+      index === self.findIndex((t) => t.fieldName === item.fieldName)
     );
 
     useEffect(() => {
@@ -46,26 +36,24 @@ const App = () => {
         dispatch(getOperatorsAddonStart());
     }, [dispatch]);
 
-    const getDynamicColumns = operatorsAddon.map(item => ({field: item.fieldName, headerName: item.fieldName, width: 260}));
-    const getDynamicRows: ColumnsType[] = operatorsAddon.map(item => ({[item.fieldName]: item.text}))
+    const dynamicColumns = useMemo(() => operatorsAddon.map(item => ({field: item.fieldName, headerName: item.fieldName, width: 260})), [operatorsAddon]);
+    const dynamicRows: ColumnsType = useMemo(() => operatorsAddon.reduce((acc, item) => ({...acc, [item.fieldName]: item.text}), {}), [operatorsAddon]);
 
-    const columns: GridColDef[] = [
-        {field: STATIC_FIELD_ID, headerName: STATIC_HEADER_NAME_ID, width: 90},
-        {field: STATIC_FIELD_NAME, headerName: STATIC_HEADER_NAME_USER, width: 150},
-        {field: STATIC_FIELD_IS_WORKING, headerName: STATIC_HEADER_NAME_WORKING, width: 150, renderCell: CheckBoxCell},
-        {field: STATIC_FIELD_CREATED_AT, headerName: STATIC_HEADER_NAME_CREATING_DATE, width: 190},
-        ...getDynamicColumns
-    ];
+    const columns: GridColDef[] = useMemo(() => [
+      ...STATIC_COLUMNS,
+      ...dynamicColumns,
+    ], [dynamicColumns]);
 
-    const rows: RowsType[] = 
-      [...operators.map(item => ({id: item.id, name: item.name, isWorking: item.isWorking, createdAt: moment(item.createdAt).format('MMMM Do YYYY, h:mm:ss')}))];
-
-    rows.forEach(obj => {
-      getDynamicRows.forEach(item => {
-          Object.assign(obj, item);
-      });
-    });
-  
+    const rows: RowsType[] = useMemo(() => {
+      return operators.map(item => ({
+        id: item.id,
+        name: item.name,
+        isWorking: item.isWorking,
+        createdAt: moment(item.createdAt).format('MMMM Do YYYY, h:mm:ss'),
+        ...dynamicRows
+      }))
+    }, [dynamicRows, operators]) 
+      
     return (
         <Wrapper><Table rows={rows} columns={columns}/></Wrapper>
     );
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
+import CheckBoxCell from '../ui/cells/Checkbox/Checkbox';
+
 // Store
 
 export const OPERATORS_SLICE_NAME: string = 'operators';
@@ -20,6 +22,12 @@ export const STATIC_HEADER_NAME_USER: string = 'Користувач';
 export const STATIC_HEADER_NAME_WORKING: string = 'Працює';
 export const STATIC_HEADER_NAME_CREATING_DATE: string = 'Дата / Час створення';
 
+export const STATIC_COLUMNS = [
+    {field: STATIC_FIELD_ID, headerName: STATIC_HEADER_NAME_ID, width: 90},
+    {field: STATIC_FIELD_NAME, headerName: STATIC_HEADER_NAME_USER, width: 150},
+    {field: STATIC_FIELD_IS_WORKING, headerName: STATIC_HEADER_NAME_WORKING, width: 150, renderCell: CheckBoxCell},
+    {field: STATIC_FIELD_CREATED_AT, headerName: STATIC_HEADER_NAME_CREATING_DATE, width: 190},
+]
 // Endpoints
 
 export const OPERATORS_ENDPOINT: string = '/operator';
